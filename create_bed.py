1ef74f8e2b09a50108bce7a63c3562ffbabc3ae4
Simon Beal <sb48@sanger.ac.uk>
2015-07-17 08:33:56 +0100

    Update create_bed

--- create_bed.py
+++ create_bed.py
@@ -2,17 +2,24 @@ TSV = "/nfs/cancer_translation/Sequenom_and_STR_analysis_of_cell_lines/Data_for_
 
 class CreateBed(object):
     def __init__(self):
+        #Open the tsv file
         tsv_f = open(TSV)
+        line_id = 0
         for line in tsv_f.readlines()[1:]:
             parts = line.split("\t")
+            #Does the line have an extra field?
             fiddle = not bool(parts[18].find(":")+1)
             chromosome_id, pos = parts[18+fiddle].split(":")
             pos = map(int, pos.split("-"))
             rep = parts[13+fiddle]
-            cosmic_id = parts[12+fiddle]
+            cosmic_id = parts[28+fiddle]
             if rep[-2] != ">": continue
             rep = rep[-3], rep[-1]
-            print chromosome_id, pos[0], rep[0], rep[1], cosmic_id
+            #print parts
+            print "\t".join(map(str, (chromosome_id, pos[0], rep[0], rep[1])))
+            if line_id == -1:
+                exit()
+            line_id+=1
         tsv_f.close()
 
 def main(): CreateBed()
