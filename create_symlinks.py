a9dd4059ee87065fe79f7f952eb5920d865f4330
Simon Beal <sb48@sanger.ac.uk>
2015-07-21 09:31:01 +0100

    Update create_symlinks

--- create_symlinks.py
+++ create_symlinks.py
@@ -2,14 +2,13 @@ import os, glob
 
 class Main(object):
     def __init__(self):
-        self.bam_dir = "1085_out"
-        self.exome_dir = "758_and_786_out"
+        self.exome_dir = "vcf_758_786"
         map(self.parse_sample, glob.glob(os.path.join(self.exome_dir, "*.vcf.gz*")))
     
     def parse_sample(self, src):
         sample_name = os.path.basename(src)
-        dst = os.path.join(self.bam_dir, sample_name)
-        print src, dst
+        dst_name = sample_name.split(".")[0]+"_RNA_TCGA."+".".join(sample_name.split(".")[1:])
+        dst = os.path.join(self.exome_dir, dst_name)
         try:
             os.unlink(dst)
         except OSError: pass
@@ -21,5 +20,3 @@ def main(): Main()
 
 if __name__ == "__main__":
     main()
-
-"/software/CGP/projects/vcfCommons/perl/bin/mergeAndPileup.pl -i 1085_out/1085_VcfMergeConfig.ini -d 1085_out/ -a snp -s 1 -o 1085_vcfout  -f cave_java &"
